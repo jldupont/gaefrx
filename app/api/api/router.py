2ec2027dd733eec6808b0b3f447097de5c11eac8
github@jldupont.com <github@jldupont.com>
2015-06-21 12:38:15 -0400

    common sources

--- app/api/api/router.py
+++ app/api/api/router.py
@@ -1,14 +1,26 @@
-
-import os, logging #@UnusedImport
+'''
+    API router
+    
+    @author: Jean-Lou Dupont
+'''
+import os, sys, logging #@UnusedImport
 import webapp2
+
+dn = os.path.dirname
+
+##
+##  Update python path with 'src_common' directory
+##
+two_dirs_up = dn(dn(__file__))
+sys.path.append(os.path.join(two_dirs_up, 'src_common'))
 
 
 class RouterApp(webapp2.RequestHandler):
     
-    def get(self, *p, **k):
+    def get(self, *p):
         '''
         '''
-        logging.info("API GET: %s %s" % (p, k))
+        logging.info("API GET: %s" % (p, ))
 
 
 app = webapp2.WSGIApplication([
---
+++ src_common/pyrbac/control.py
@@ -0,0 +1,53 @@
+'''
+Access Control
+
+Created on Jun 14, 2015
+
+@author: jldupont
+'''
+
+from .user import UserRBAC
+from .permission import Permission, PermissionError
+
+def ensure(user, permission):
+    '''
+    Ensures that 'user' has 'permission'
+    
+    Go through all the 'roles' assigned to a user type and
+     confirm that there is at least 1 of these roles that
+     list the required 'permission'. Else, raise 'PermissionError'.
+
+    @param user: subclass of BaseUserRBAC
+    @param permission: subclass of Permission  
+    
+    @return: None
+    @raise PermissionError 
+    '''
+    assert isinstance(user, UserRBAC) or issubclass(user, UserRBAC),     "The parameter 'user' must be an instance of 'BaseUserRBAC'"
+    assert isinstance(permission, Permission), "The parameter 'permission' must be an instance of 'Permission'"
+    
+    for role in user.roles:
+        if role.has_all_permissions:
+            return
+        
+        if has_permission(role, permission):
+            return
+        
+    raise PermissionError('Expecting %s' % permission)
+    
+
+def has_permission(role, permission):
+    '''
+    Verifies that 'role' has 'permission'
+    
+    @param role: subclass of Role
+    @param permission: subclass of Permission  
+    
+    @return True | False
+    '''
+    
+    for _permission in role.permissions:
+        if _permission == permission:
+            return True
+
+    return False
